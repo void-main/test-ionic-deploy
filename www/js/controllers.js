f23af9eacc43aec8676926504a7d2e798c9d191e
Void Main <voidmain1313113@gmail.com>
2015-06-06 09:53:33 +0800

    dummy project

---
+++ www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $ionicDeploy) {
+      // Update app code with new release from Ionic Deploy
+      $scope.doUpdate = function() {
+        $ionicDeploy.update().then(function(res) {
+          console.log('Ionic Deploy: Update Success! ', res);
+        }, function(err) {
+          console.log('Ionic Deploy: Update error! ', err);
+        }, function(prog) {
+          console.log('Ionic Deploy: Progress... ', prog);
+        });
+      };
+
+      // Check Ionic Deploy for new code
+      $scope.checkForUpdates = function() {
+        console.log('Ionic Deploy: Checking for updates');
+        $ionicDeploy.check().then(function(hasUpdate) {
+          console.log('Ionic Deploy: Update available: ' + hasUpdate);
+          $scope.hasUpdate = hasUpdate;
+        }, function(err) {
+          console.error('Ionic Deploy: Unable to check for updates', err);
+        });
+      }
+    })
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
